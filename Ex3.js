393e11c1f4b40ebb5a7690482ff0eb733474d713
Thiago Moura <thiago.moura@users.noreply.github.com>
2020-08-20 01:44:19 -0300

    Ex_04

--- Ex3.js
+++ Ex3.js
@@ -4,6 +4,7 @@ let chess = " "
 for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
         if ((x + y ) % 2 == 0){
+            chess += " "
         }
         else {
             chess += "#"
---
+++ Ex4.js
@@ -0,0 +1,9 @@
+//MÁXIMO
+//O capítulo anterior introduziu a função Math.min que retorna o seu menor argumento. Nós podemos reproduzir essa funcionalidade agora. Escreva uma função max que recebe dois argumentos e retorna o maior deles.
+// função math.min?
+
+function Math(x){
+    return (6,9)
+}
+    console.log(Math(6,9))
+
